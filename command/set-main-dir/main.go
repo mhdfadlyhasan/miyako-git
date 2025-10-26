5c9ccba443a8fee2144b4dc390de7333298ca119
mhdfadlyhasan <mhdfadlyhasan16a1@gmail.com>
2025-10-26 13:26:42 +0700

    add assign main dir

--- command/set-main-dir/main.go
+++ command/set-main-dir/main.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -9,4 +10,10 @@ import (
 var SetMainDir = func(cmd *cobra.Command, args []string) {
 	fmt.Println("this will set main dir!")
 
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error!:", err)
+		return
+	}
+	fmt.Println("Current working directory:", dir)
 }
